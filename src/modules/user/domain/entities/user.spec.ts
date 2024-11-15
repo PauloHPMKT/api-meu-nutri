336c96499fcef20c56aa8c9a4f14032bd98e1406
Paulo Sergio <pauloserg861@gmail.com>
2024-11-15 11:25:12 -0300

    test: ensure isActive is true when it is not provided

--- src/modules/user/domain/entities/user.spec.ts
+++ src/modules/user/domain/entities/user.spec.ts
@@ -1,21 +1,27 @@
 import { User } from "./User"
 
+const makeSut = (): User => {
+    const sut = new User({
+        name: 'John Doe',
+        email: 'any_email@example.com',
+        password: 'any_password'    
+    })
+    return sut
+}
+
 describe('User entity', () => {
     it('should be defined', () => {
-        const userEntity = new User({
-            name: 'John Doe',
-            email: 'any_email@example.com',
-            password: 'any_password'
-        })
-        expect(userEntity).toBeDefined()
+        const sut = makeSut()
+        expect(sut).toBeDefined()
     })
 
     it('should define avatar as null when it is not provided', () => {
-        const userEntity = new User({
-            name: 'John Doe',
-            email: 'any_email@example.com',
-            password: 'any_password'
-        })
-        expect(userEntity.avatar).toBeNull()
+        const sut = makeSut()
+        expect(sut.avatar).toBeNull()
+    })
+
+    it('should define isActive as true when it is not provided', () => {
+        const sut = makeSut()
+        expect(sut.isActive).toBeTruthy()
     })
 })
